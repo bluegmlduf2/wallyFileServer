ca2092e5aa7e046e842696704240aa5b75e06513
wally_yoon <bluegmlduf2@naver.com>
2023-02-28 23:35:50 +0900

    add: XSS 공격방지용 CSRF를 form태그에 추가

--- resources/views/file/create.blade.php
+++ resources/views/file/create.blade.php
@@ -7,14 +7,16 @@
     </x-slot>
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="mx-4 sm:p-8">
-            <form enctype="multipart/form-data">
+            <form method="post" action="{{route('file.store')}}" enctype="multipart/form-data">
+                {{-- XSS방지를 위해서 CSRF를 추가한다(XSS란 유저가 요청한 페이지에 악성스크립트를 삽입해서 악의적동작이 실행되는 공격) --}}
+                {{-- Laravel의 Form태그는 CSRF가 필수이다 --}}
+                @csrf
                 <div class="md:flex items-center mt-8">
                     <div class="w-full flex flex-col">
-                    <label for="title" class="font-semibold leading-none mt-4">파일명</label>
-                    <input type="text" name="title" class="w-auto py-2 placeholder-gray-300 border border-gray-300 rounded-md" id="title" placeholder="파일명을 입력해주세요">
+                    <label for="filename" class="font-semibold leading-none mt-4">파일명</label>
+                    <input type="text" name="filename" class="w-auto py-2 placeholder-gray-300 border border-gray-300 rounded-md" id="filename" placeholder="파일명을 입력해주세요">
                     </div>
                 </div>
-
                 <div class="w-full flex flex-col">
                     <label for="image" class="font-semibold leading-none mt-4">선택된 파일</label>
                     <div>
