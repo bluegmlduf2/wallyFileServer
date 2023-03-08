ed5821968b971b52380a30f0c4da10c84a5a8862
wally_yoon <bluegmlduf2@naver.com>
2023-03-08 21:33:30 +0900

    add: 헬퍼함수작성및등록

---
+++ app/helper.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * 바이트 형식을 변환해주는 함수
+ */
+function formatBytes($bytes, $precision = 2) { 
+    $units = array('B', 'KB', 'MB', 'GB', 'TB'); 
+
+    $bytes = max($bytes, 0); 
+    $pow = floor(($bytes ? log($bytes) : 0) / log(1024)); 
+    $pow = min($pow, count($units) - 1); 
+
+    // Uncomment one of the following alternatives
+    // $bytes /= pow(1024, $pow);
+    // $bytes /= (1 << (10 * $pow)); 
+
+    return round($bytes, $precision) . ' ' . $units[$pow]; 
+} 
